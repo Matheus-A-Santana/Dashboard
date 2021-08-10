0384f817db9353f841302ab5467b5591f1283f14
Matheus Augusto <57044054+Matheus-A-Santana@users.noreply.github.com>
2021-08-10 01:36:04 -0300

    databases connecting
    começando a fazer as conexões no banco de dados e criei a camada transfer(DTO)

---
+++ DataBase/DataBase/Config/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Config
+{
+    class Connection
+    {
+        SqlConnection con = new SqlConnection();
+
+        private Connection()
+        {
+            con.ConnectionString = ConfigurationManager.AppSettings["SqlConnection"];
+        }
+
+        public SqlConnection Connecting()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            else if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            return con;
+        }
+    }
+}
--- DataBase/DataBase/Properties/Settings.Designer.cs
+++ DataBase/DataBase/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace DataBase.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-LRIAAJB;Initial Catalog=Dashboard;Integrated Security=True")]
-        public string DashboardConnectionString {
-            get {
-                return ((string)(this["DashboardConnectionString"]));
-            }
-        }
     }
 }
